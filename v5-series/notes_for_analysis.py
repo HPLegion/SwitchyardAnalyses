f4a3939fd2e6a3702bc03c4f812044d5ac6f5444
HPLegion <32222819+HPLegion@users.noreply.github.com>
2017-11-21 16:32:11 +0100

    backup

--- v5-series/notes_for_analysis.py
+++ v5-series/notes_for_analysis.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pandas as pd
+import altair as alt
+
 import PhaseSpaceEval.monitor_quantities as monq
 from PhaseSpaceEval.import_particle_data import *
 from PhaseSpaceEval.trajectory import Trajectory
@@ -99,6 +101,40 @@ missesBySrc = dict()
 eventsBySrc = dict()
 for sID in sourceIDs:
     missesBySrc.update({sID : monBySrc[sID].get_misses()})
-    eventsBySrc.update({sID : monBySrc[sID].get_events()})
-    
+    eventsBySrc.update({sID : monBySrc[sID].get_events()}) 
 #print(missesBySrc)
+
+# Add lost particles to miss counter
+for pID in lostParticles:
+    for sID in sourceIDs:
+        if pID in particlesBySrc[sID]:
+            missesBySrc[sID] += 1
+#print(missesBySrc)
+
+
+# Compute and save emittances
+
+colnames = ["sourceID", "sourceName", "x_offset", "y_offset", "x_emittance", "y_emittance", "losses"]
+emit_df = pd.DataFrame(columns=colnames)
+emit_temp = pd.DataFrame([np.zeros(len(colnames))], columns=colnames)
+for sID in sourceIDs:
+    name = particle_source_names[sID]
+    xoff = float(name.split('_')[1])
+    yoff = float(name.split('_')[3])
+    xemit = monq.emittance_u(eventsBySrc[sID])
+    yemit = monq.emittance_v(eventsBySrc[sID])
+    losses = missesBySrc[sID]
+    
+    emit_temp["sourceID"] = sID
+    emit_temp["sourceName"] = name
+    emit_temp["x_offset"] = xoff
+    emit_temp["y_offset"] = yoff
+    emit_temp["x_emittance"] = xemit
+    emit_temp["y_emittance"] = yemit
+    emit_temp["losses"] = losses
+
+    emit_df = emit_df.append(emit_temp, ignore_index=True)
+
+emit_df.sort_values(["x_offset", "y_offset"], inplace=True)
+emit_df.reset_index(inplace=True, drop=True)
+emit_df.to_csv(EMIT_FILENAME)
